313a15496adf7d7807602ea3480f336f380024c5
= <luke.metz@students.olin.edu>
2015-02-17 00:38:22 -0500

    moved to cuboid

--- batch_norm/simple.py
+++ batch_norm/simple.py
@@ -24,115 +24,8 @@ from blocks.extensions import Printing, Timing
 from blocks.extensions import SimpleExtension
 from blocks.extensions.monitoring import DataStreamMonitoring
 
-class BatchNormalizationBase(Brick):
-
-    seed_rng = np.random.RandomState(config.default_seed)
-
-    @lazy
-    def __init__(self, B_init, Y_init, epsilon=1e-9, seed=None, **kwargs):
-        super(BatchNormalizationBase, self).__init__(**kwargs)
-        self.eps = epsilon
-        self.seed = seed
-        self.B_init = B_init
-        self.Y_init = Y_init
-
-    @property
-    def seed(self):
-        if getattr(self, '_seed', None) is not None:
-            return self._seed
-        else:
-            self._seed = self.seed_rng.randint(np.iinfo(np.int32).max)
-            return self._seed
-
-    @seed.setter
-    def seed(self, value):
-        if hasattr(self, '_seed'):
-            raise AttributeError("seed already set")
-        self._seed = value
-
-    @property
-    def rng(self):
-        if getattr(self, '_rng', None) is not None:
-                return self._rng
-        else:
-            return np.random.RandomState(self.seed)
-
-    @rng.setter
-    def rng(self, rng):
-        self._rng = rng
-
-    def _initialize(self):
-        B, Y = self.params
-        self.B_init.initialize(B, self.rng)
-        self.Y_init.initialize(Y, self.rng)
-
-
-class BatchNormalizationConv(BatchNormalizationBase):
-    @lazy
-    def __init__(self, input_shape, **kwargs):
-        super(BatchNormalizationConv, self).__init__(**kwargs)
-        self.input_shape = input_shape
-        self.num_channels = input_shape[0]
-
-    def _allocate(self):
-        B = shared_floatx_zeros((self.num_channels,))
-        self.params.append(B)
-
-        Y = shared_floatx_zeros((self.num_channels,))
-        self.params.append(Y)
-
-    @application(inputs=['input_'], outputs=['output'])
-    def apply(self, input_):
-        """
-        Reference: http://arxiv.org/pdf/1502.03167v2.pdf
-        """
-        minibatch_mean = T.mean(input_, axis=[0, 2, 3]).dimshuffle('x', 0, 'x', 'x')
-        minibatch_var = T.var(input_, axis=[0, 2, 3]).dimshuffle('x', 0, 'x', 'x')
-        norm_x = (input_ - minibatch_mean) / (T.sqrt(minibatch_var + self.eps))
-        B, Y = self.params
-        B = B.dimshuffle(('x', 0, 'x', 'x'))
-        Y = Y.dimshuffle(('x', 0, 'x', 'x'))
-        return norm_x * Y + B
-
-    def get_dim(self, name):
-        if name == "input_":
-            return self.input_shape
-        if name == "output":
-            return self.input_shape
-        super(BatchNormalizationConv, self).get_dim(name)
-
-class BatchNormalization(BatchNormalizationBase):
-    @lazy
-    def __init__(self, input_dim, **kwargs):
-        super(BatchNormalization, self).__init__(**kwargs)
-        self.input_dim = input_dim
-
-    def _allocate(self):
-        B = shared_floatx_zeros((self.input_dim,))
-        self.params.append(B)
-
-        Y = shared_floatx_zeros((self.input_dim,))
-        self.params.append(Y)
-
-    @application(inputs=['input_'], outputs=['output'])
-    def apply(self, input_):
-        """
-        Reference: http://arxiv.org/pdf/1502.03167v2.pdf
-        """
-        minibatch_mean = T.mean(input_, axis=[0]).dimshuffle('x', 0)
-        minibatch_var = T.var(input_, axis=[0]).dimshuffle('x', 0)
-        norm_x = (input_ - minibatch_mean) / (T.sqrt(minibatch_var + self.eps))
-        B, Y = self.params
-        B = B.dimshuffle(('x', 0))
-        Y = Y.dimshuffle(('x', 0))
-        return norm_x * Y + B
-
-    def get_dim(self, name):
-        if name == "input_":
-            return self.input_dim
-        if name == "output":
-            return self.input_dim
-        super(BatchNormalizationConv, self).get_dim(name)
+from cuboid.algorithms import AdaM
+from cuboid.bricks import BatchNormalizationConv, BatchNormalization
 
 X = T.matrix("features")
 
@@ -164,8 +57,6 @@ l = MaxPooling(pooling_size=(2, 2),
         input_dim=l.get_dim("output"))
 l.initialize()
 o = l.apply(o)
-
-#o = Rectifier().apply(o)
 
 l = Convolutional(filter_size=(3, 3),
         num_filters=32,
@@ -220,37 +111,6 @@ cost.name = "cost"
 
 miss_class = 1.0 - MisclassificationRate().apply(Y.flatten(), o)
 miss_class.name = "accuracy"
-
-class AdaM(StepRule):
-    def __init__(self, alpha=0.0002, b1=0.1, b2=0.001, eps=1e-8):
-        self.alpha = alpha
-        self.b1 = b1
-        self.b2 = b2
-        self.eps=eps
-
-    def compute_step(self, p, g):
-        b1 = self.b1
-        b2 = self.b2
-        eps = self.eps
-        alpha = self.alpha
-
-        m = theano.shared(p.get_value() * 0.)
-        v = theano.shared(p.get_value() * 0.)
-        t = theano.shared(np.array(0.0, dtype=theano.config.floatX))
-
-        t_new = t + 1.0
-        m_new = b1 * g + (1.0 - b1) * m
-        v_new = b2 * g * g + (1 - b2) * v
-
-        m_hat = m_new / (1.0 - T.pow((1.0 - b1), t_new))
-        v_hat = v_new / (1.0 - T.pow((1.0 - b2), t_new))
-
-        step = alpha * m_hat / (T.sqrt(v_hat) + eps)
-        updates = [(m, m_new), (v, v_new), (t, t_new)]
-
-        return step, updates
-
-print "Learning rules"
 
 step_rule = AdaM()
 algorithm = GradientDescent(cost=cost, step_rule=step_rule)
